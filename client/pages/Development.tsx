1e14d9551d23f2e826daba9720e1fb73fa222b9b
Ija Maharyta <ijamaharyta@gmail.com>
2025-08-28 23:46:24 +0300

    chore: typo fix

--- client/pages/Development.tsx
+++ client/pages/Development.tsx
@@ -20,7 +20,7 @@ export default function Development() {
           Ця сторінка поки недоступна
         </p>
         <p className="text-xs text-black/50 font-medium">
-          Очікуйте кінця етапу 4 вересня !
+          Очікуйте на відповідний етап!
         </p>
       </div>
     </div>
