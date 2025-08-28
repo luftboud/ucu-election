4553171518cfe4ccb3a7ee78cb71a406cae3eebc
Ija Maharyta <ijamaharyta@gmail.com>
2025-08-28 23:41:03 +0300

    fix: footer logos responsivity

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -5,21 +5,26 @@ export default function Footer() {
     <footer className="bg-dark-blue text-ivory">
       <div className="container mx-auto px-4 py-6">
         <div className="flex justify-between items-center">
-          <div className="text-left">
+          <div className="text-left w-[70%] md:w-auto">
             <p className="text-sm mb-2">
               © {currentYear} Студентська виборча комісія УКУ
             </p>
             <a
               href="https://send.monobank.ua/jar/8TMyF3hSBb"
-              className="text-xs text-gray-300"
+              className="text-xs text-gray-300 block"
               target="_blank"
             >
-              Сподобалась система голосування? Тоді <b>надішліть кілька гривень,</b> щоб купити кавусі для команди
+              Сподобалась система голосування? Тоді{" "}
+              <b>надішліть кілька гривень,</b> щоб купити кавусі для команди
             </a>
           </div>
 
-          <div className="flex space-x-4 items-center">
-            <img src="images/logo_svk.png" alt="logo СВК" className="h-10" />
+          <div className="flex space-y-2 md:space-y-0 md:space-x-4 items-center md:flex-row flex-col">
+            <img
+              src="images/logo_svk.png"
+              alt="logo СВК"
+              className="h-6 md:h-10"
+            />
             <a
               href="https://www.instagram.com/stcouncilucu?igsh=MWkwYzU3MGRieXhkMw=="
               target="_blank"
@@ -27,7 +32,7 @@ export default function Footer() {
               <img
                 src="images/logo_scouncil.png"
                 alt="logo student council"
-                className="h-16"
+                className="h-12 md:h-16"
               />
             </a>
           </div>
