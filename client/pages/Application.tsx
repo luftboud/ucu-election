91df9ce4bcb309471dfba0b8a230c23d13b82ea2
Ija Maharyta <ijamaharyta@gmail.com>
2025-09-09 20:25:18 +0300

    update voting closed

--- client/pages/Application.tsx
+++ client/pages/Application.tsx
@@ -63,23 +63,23 @@ export default function Application() {
           {/* Applications closed */}
 
           <h1 className="text-4xl md:text-5xl font-bold mb-8">
-            Триває голосування!
+            Очікуйте на результати!
           </h1>
           <p className="text-lg text-ivory/80 mb-12 font-medium">
-            Кінець голосування: 9 вересня, 18:00
+            Публікація результатів: 10 вересня, 10:00
           </p>
           <img
             className="h-24 mx-auto mb-8"
             src="images/application-ended.webp"
             alt="a cat looking"
           />
-          <Link to={`/voting`}>
+          {/* <Link to={`/voting`}>
             <button
               className="bg-ivory text-dark-blue px-10 py-3 text-lg font-semibold rounded-lg hover:bg-gray-100 transition-all duration-300 hover:scale-105 hover:shadow-lg"
             >
               Голосувати
             </button>
-          </Link>
+          </Link> */}
         </div>
         {/* Scroll Down Button */}
         <div className="absolute bottom-[11%] left-0 right-0 flex justify-center">
