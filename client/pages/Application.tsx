3b731b979e841df4df1edaf034dd0665dbff8d60
Ija Maharyta <ijamaharyta@gmail.com>
2025-09-11 12:47:34 +0300

    chore: arrow position change

--- client/pages/Application.tsx
+++ client/pages/Application.tsx
@@ -82,7 +82,7 @@ export default function Application() {
           </Link> */}
         </div>
         {/* Scroll Down Button */}
-        <div className="absolute bottom-[11%] left-0 right-0 flex justify-center">
+        <div className="absolute bottom-[5%] left-0 right-0 flex justify-center">
           <button
             onClick={() => {
               const nextSection = document.querySelector(
